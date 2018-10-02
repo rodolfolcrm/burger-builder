022f446015bae2f1bf05914f8e93872264591808
rodolfo.dacunha <Juvenal4043>
2018-10-02 10:51:14 +0200

    refactoring folders.

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react';
 
-import Aux from '../../hoc/Auxiliary';
+import Aux from '../Auxiliary/Auxiliary';
 import classes from './Layout.css';
 
-import Toolbar from './../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
 
 class Layout extends Component {
 
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {
